521f357aabbfe2d985365139d942ee53ed285921
BlackHat86 <suryansh.c05@gmail.com>
2026-01-18 13:01:42 +0530

    again changed providers.jsx

--- app/providers.jsx
+++ app/providers.jsx
@@ -15,7 +15,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function Providers({ children }) {
     return (
-        <GoogleOAuthProvider clientId="509482865609-kkcnqatpc8sg91tnval0gp94sm8b417h.apps.googleusercontent.com">
+        <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
             {children}
         </GoogleOAuthProvider>
     );
